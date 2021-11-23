28a81dde48a3c04469047f9ba70a1ee177410e8b
fcbuss <fcbuss@outlook.com>
2021-11-23 09:56:25 +0100

    ohne plugins Fehler

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,28 +14,4 @@ module.exports = {
   flags: {
     FAST_DEV: true,
   },
-  plugins: [
-    {
-      resolve: "gatsby-source-shopify",
-      //options: {
-      //  password: process.env.SHOPIFY_SHOP_PASSWORD,
-      //  storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
-      //  shopifyConnections: ["collections"],
-      //},
-    },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-gatsby-cloud",
-    // Add your Google Analytics ID to the .env file to enable
-    // Otherwise, this plugin can be removed
-    process.env.GOOGLE_ANALYTICS_ID && {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
-  ].filter(Boolean),
 }
